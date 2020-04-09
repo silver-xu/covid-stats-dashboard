a7ec0ecc4d2215f3539ca41eb49bfd7e98d40b64
Silver Xu <me@silverxu.com>
2020-04-09 21:22:00 +1000

    Add Australian map marker

--- src/components/Australia/AustraliaMap.tsx
+++ src/components/Australia/AustraliaMap.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography, ZoomableGroup, Marker } from 'react-simple-maps';
 
 import { ParentStats, Metrics } from '../../types/stats';
 import { default as countries } from '../../config/countries.json';
 import { colorScale } from '../../utils/colorScale';
+import { geoCentroid } from 'd3';
 
 const geoUrl = 'au-states-topo.json';
 const statesGeoLookup = Object.assign(
@@ -24,19 +25,35 @@ export const AustraliaMap = ({ australiaStats, metrics }: { australiaStats: Pare
     >
       <ZoomableGroup center={[132, -28]} zoom={4}>
         <Geographies geography={geoUrl} fill="#DDD">
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const statesCode = statesGeoLookup[geo.properties.name];
-              const data = australiaStats[statesCode];
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={data ? colorScale(data[metrics], 'Australia') : '#EEE'}
-                />
-              );
-            })
-          }
+          {({ geographies }) => (
+            <>
+              {geographies.map((geo) => {
+                const statesCode = statesGeoLookup[geo.properties.name];
+                const data = australiaStats[statesCode];
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={data ? colorScale(data[metrics], 'Australia') : '#EEE'}
+                  />
+                );
+              })}
+              {geographies.map((geo) => {
+                const centroid = geoCentroid(geo);
+                return (
+                  <g key={geo.rsmKey + '-name'}>
+                    {
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={2} textAnchor="middle" fill="#555">
+                          {geo.properties.name}
+                        </text>
+                      </Marker>
+                    }
+                  </g>
+                );
+              })}
+            </>
+          )}
         </Geographies>
       </ZoomableGroup>
     </ComposableMap>
