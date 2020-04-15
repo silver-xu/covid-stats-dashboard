fa9646830446e9872ee861b17d489a59e6093528
silverxu <me@silverxu.com>
2020-04-15 18:11:13 +1000

    Fixing build

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { GraphQLClient, ClientContext } from "graphql-hooks";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GraphQLClient, ClientContext } from 'graphql-hooks';
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const client = new GraphQLClient({
-  url: "https://api.covidstats.com.au/graphql",
+  url: 'https://api.covidstats.com.au/graphql',
 });
 
 ReactDOM.render(
   <ClientContext.Provider value={client}>
     <App />,
   </ClientContext.Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
